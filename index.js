2eb9ca9f36dd57a503e6581a62ea4f10c250498e
u4bi <u4bi@u4bi.com>
2017-03-01 13:41:39 +0000

    intro page component render

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-var app = angular.module('phpAngular',['ngRoute']);
+var app = angular.module('phpAngular',['ngRoute','angularCSS']);
 
 app.config(function($routeProvider) {
   $routeProvider
   .when('/', {
+    templateUrl: './views/intro/intro.html',
+    controller: 'MainController',
+  })
+  .when('/main', {
     templateUrl: './views/main/main.html',
-    controller: 'MainController'
+    controller: 'MainController',
+    css : './css/style.css'
   });
 });
